d469de1f7c327ef6f74d61e747469d9fb818ab38
MartaLokit <93724240+MartaLokit@users.noreply.github.com>
2021-11-25 18:25:43 +0300

    Задание 1

---
+++ Задание 1/Задание 1.2/ISweetRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_1._2
+{
+    interface ISweetRepository
+    {
+        string Read();
+        
+    }
+}
