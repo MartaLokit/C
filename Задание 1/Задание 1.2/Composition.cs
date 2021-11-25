9be3b77fefc6e6d04b8be7f00d771cd66c1b4766
MartaLokit <93724240+MartaLokit@users.noreply.github.com>
2021-11-25 18:27:58 +0300

    Задание 1

---
+++ Задание 1/Задание 1.2/Composition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Задание_1._2.Users;
+
+namespace Задание_1._2
+{
+    class Composition
+    {
+        SweetRepository sweet = new SweetRepository();
+        public string SweetName { get; set; }
+        public int WeightSet { get; set; }
+        public int Sugar { get; set; }
+        public double Price { get; set; }
+        public string composition()
+        {
+             return sweet.Read();
+        }
+    }
+}
---
+++ Задание 1/Задание 1.2/SweetRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+namespace Задание_1._2.Users
+{
+    public class SweetRepository : ISweetRepository
+    {  
+        public string[] sweets = File.ReadAllLines("set.txt");
+        public string Read()
+        {
+            foreach (var item in sweets)
+            {
+                Console.WriteLine(item);
+            }
+            return " ";
+        }
+    }
+}
