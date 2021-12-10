cc7f7ade1d574ed1225012f3264abe6e14a11b81
MartaLokit <93724240+MartaLokit@users.noreply.github.com>
2021-12-10 23:53:12 +0300

    Задание 2

--- Задание 2/Задание 2/Busines/DocumentService.cs
+++ Задание 2/Задание 2/Busines/DocumentService.cs
@@ -1,26 +1,22 @@
 ﻿using System;
-using System.Linq;
 using System.IO;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Задание_2.Busines
 {
-    public class DocumentService: IDocumentService
-    {
+    public class DocumentService : IDocumentService
+    {    
         public string NumberWord()
         {
-            var text = File.ReadAllText("HerFault .txt");
-            var Lines = File.ReadAllLines("HerFault .txt");
+            string text = File.ReadAllText("HerFault  — копия.txt");
+            var Lines = File.ReadAllLines("HerFault  — копия.txt");
             int i = 1;
-            var allwords = text.Split(' ', '.', '\n');
-            var unique =(from word in allwords select word.ToLower()).Distinct().OrderBy(name => name);
-                foreach (var word in unique)
-                {
-                    int cnt = (from word2 in allwords where word2.ToLower() == word select word2).Count();
-                    Console.Write($"\n{word}.....");
-                    Console.Write($"{cnt}.....");
+            var allwords = text.Split(' ', '.');
+            var unique = (from word in allwords select word.ToLower()).Distinct().OrderBy(name => name);
+            foreach (var word in unique)
+            {
+                int cnt = (from word2 in allwords where word2.ToLower() == word select word2).Count();
+                Console.Write($"\n{word}.....{cnt}...");
                 for (i = 0; i < Lines.Length; i++)
                 {
                     if (Lines[i].Contains(word))
@@ -28,43 +24,29 @@ namespace Задание_2.Busines
                         Console.Write($"{i + 1},");
                     }
                 }
-
             }
-            Console.ReadLine();
             return "";
         }
         public string NumberChar()
         {
-            int cnt = 0;
-            int i = 0;
-            var firstLetters = "";
-            string[] text = File.ReadAllLines("HerFault .txt");
-            var Lines = File.ReadAllLines("HerFault .txt");
-            var sort = text.OrderBy(s => s);
-            foreach (var item in text)
+            string firstLetters;
+            var word = File.ReadAllText("HerFault  — копия.txt");
+            var Lines = File.ReadAllLines("HerFault  — копия.txt");
+            var allwords = word.Split(' ', ',', '\n');
+            var linq = (from words in allwords select words.ToLower()).Distinct().OrderBy(name => name);
+            foreach (var item in linq)
             {
+                char[] s = item.ToCharArray();
                 firstLetters = new String(item.Split(' ').Select(x => x[0]).ToArray());
-            }         
-            foreach (var item in text)
-            {
-                string[] Split = item.Split(new[] { ' ', ',','\n' });
-                var linq = (from word in Split select word.ToLower()).Distinct();
-                foreach (string word in linq)
+                if (s[0] == firstLetters[0])
                 {
-                    cnt = (from word2 in Split where word2.ToLower() == word select word2).Count();
-                    for (int k = 0; k < text.Length; k++)
+                    var cnt = (from word2 in allwords where word2.ToLower() == item select word2).Count();
+                    Console.Write($"\n{item}......{cnt}.....");
+                    for (int i = 0; i < Lines.Length; i++)
                     {
-                        if (word[0] == firstLetters[k])
+                        if (Lines[i].Contains(item))
                         {
-                            Console.WriteLine($"\n{word}...............");
-                            Console.Write($"{cnt}...");
-                        }
-                    }                   
-                    for (i = 0; i < text.Length; i++)
-                    {
-                        if (Lines[i].Contains(word))
-                        {
-                            Console.Write($"{i+1},");
+                            Console.Write($"{i + 1},");
                         }
                     }
                 }
