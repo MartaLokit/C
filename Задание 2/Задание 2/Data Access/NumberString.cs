891c1f3167d1c769cb88602d4697aa53e6a563e5
MartaLokit <93724240+MartaLokit@users.noreply.github.com>
2021-12-13 01:04:16 +0300

    Задание 2

---
+++ Задание 2/Задание 2/Data Access/NumberString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_2.Data_Access
+{
+   public class NumberString:GetWord
+    {
+        public string[] Lines = File.ReadAllLines("HerFault  — копия.txt");
+        public void numberWord(string word)
+        {
+            int i = 0;
+            for ( i = 0; i < Lines.Length; i++)
+            {
+                if (Lines[i].Contains(word))
+                {
+                    Console.Write(i + 1+",");  
+                }
+            }
+            
+        }
+        public void numberLetter(string item)
+        {
+            var allwords = Word().Split(' ', '.', '\t', ',');
+            char[] s = item.ToCharArray();
+            var firstLetters = new String(item.Split(' ').Select(x => x[0]).ToArray());
+            for (int i = 0; i < Lines.Length; i++)
+            {
+                if (s[0] == firstLetters[0])
+                {
+                    if (Lines[i].Contains(item))
+                    {
+                        Console.Write($"{i + 1},");
+                    }
+                }
+            }
+         }
+    }
+}
